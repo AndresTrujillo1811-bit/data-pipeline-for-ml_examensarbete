b8f14a11c3a42c7152554153813ea798bd24637e
AndresTrujillo1811-bit <andresgtrujillof@gmail.com>
2025-11-17 19:32:13 +0100

    Fixing ETL

--- etl_code/etl_pipeline.py
+++ etl_code/etl_pipeline.py
@@ -1,23 +1,56 @@
 # ETL
 import pandas as pd 
-
+import os 
 
 # Extract 
-def get_data(filepath):
-    df = pd.read_csv(filepath)  #Loading raw data from csv file
-    return df
+def get_data(input_file="etl_code/hemnet_data_clean.csv"):
+    os.makedirs('etl_code/data_ml', exist_ok=True)
+    df = pd.read_csv("etl_code/hemnet_data_clean.csv")
+    return df #Loading raw data from csv file
     
 # Transform 
 def transform_data(df):
-    try: 
+    try:            
         df = df.dropna(subset=['final_price', 'commune']) # Removing rows with missing columns
-        df['sale_date'] = pd.to_datetime(df['sale_date'], errors='coerce') # Transforming column date to datetime
+        df['sale_date'] = pd.to_datetime(df['sale_date'], format="%Y-%m-%d" ,errors='coerce') # Transforming column date to datetime
         df['month'] = df['sale_date'].dt.month
         df['year'] = df['sale_date'].dt.year
-        df = pd.get_dummies(df, columns=['area', 'commune'], drop_first=True) # Area and commune
-        
+        df = pd.get_dummies(df, columns=['area'], drop_first=True)
+        df = df.drop(columns=df.select_dtypes(include=['bool']).columns)
+        df = df.drop(columns=[ 
+            'coordinate', 
+            'sale_date',
+            'asked_price', 
+            'pourcentage_difference', 
+            'land_area',
+            'month',
+            'year',
+            'supplemental_area',
+            'price_per_area'
+        ])
+        return df
     except Exception as e:
         print(f"An error ocurred: {e}")
+        
+        
+# Load 
+def load_csv(df, output_file):
+    df.to_csv(output_file, index=False) # Cleaned data
+    print(f"Cleaned data saved to {output_file}")
+    
+    
+def main():
+    input_file = "etl_code/hemnet_data_clean.csv"
+    output_file = "etl_code/data_ml/house_price.csv"
+    data = get_data(input_file)
+    data = transform_data(data)
+    load_csv(data, output_file)
+    print("Process completed!")
+    
+if __name__ == '__main__':
+    main()    
+        
+            
         
          
     
