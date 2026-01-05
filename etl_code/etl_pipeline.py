cddf9563151cdb54ab8d0d97750ef8bd1008f628
AndresTrujillo1811-bit <andresgtrujillof@gmail.com>
2026-01-05 18:43:23 +0100

    finishing storytelling graphs for presentation

--- etl_code/etl_pipeline.py
+++ etl_code/etl_pipeline.py
@@ -28,7 +28,7 @@ def transform_data(df):
 
     days = np.random.randint(0, (end - start).days, size=len(df))
     df['sale_date'] = df['sale_date'] = start + pd.to_timedelta(days, unit='D')
-    df['status'] = np.where(np.random.rand(len(df)) > 0.62, "Available", "Sold") 
+    df['status'] = np.where(np.random.rand(len(df)) > 0.62, "On sale", "Sold") 
     #df['status'] = df['final_price'].apply(lambda x: "Available" if pd.isna(x) else "Sold")
     
     df = df.drop(columns=df.select_dtypes(include=['bool']).columns)
